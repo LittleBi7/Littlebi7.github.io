47df99845f74cc1d31460b9fcabbadb35c212fbd
PWINNER <pwinnerconi@gmail.com>
2023-09-19 10:10:18 -0600

    volviendo a agregar1 agregar

--- java.js/script.js
+++ java.js/script.js
@@ -1,29 +1,29 @@
 // script.js
 
 // Función que se ejecuta cuando se hace clic en un botón "Añadir al Carrito"
-function agregarAlCarrito1(event) {
+function agregarAlCarrito(event) {
     const producto = event.target.getAttribute("data-producto");
     alert(`${producto} agregado al carrito`);
 }
 
 // Obtener todos los botones "Añadir al Carrito" por su clase
-const botonesAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito1");
+const botonesAgregarAlCarrito = document.querySelectorAll(".agregarAlCarrito");
 
 // Agregar un evento de clic a cada botón
 botonesAgregarAlCarrito.forEach((boton) => {
-    boton.addEventListener("click", agregarAlCarrito1);
+    boton.addEventListener("click", agregarAlCarrito);
 });
 // script.js
 
 const carrito = [];
 
-function agregarAlCarrito1(event) {
+function agregarAlCarrito(event) {
     const producto = event.target.getAttribute("data-producto");
     carrito.push(producto);
-    actualizarCarrito1();
+    actualizarCarrito();
 }
 
-function actualizarCarrito1() {
+function actualizarCarrito() {
     const carritoLista = document.getElementById("carrito");
     carritoLista.innerHTML = ""; // Limpiar la lista antes de actualizar
 
