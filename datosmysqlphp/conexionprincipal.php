1cee51dc4bb262ad2654b8e053bd1e9416366988
PWINNER <pwinnerconi@gmail.com>
2023-10-02 23:47:27 -0600

    insertar y buscar registros php

--- datosmysqlphp/conexionprincipal.php
+++ datosmysqlphp/conexionprincipal.php
@@ -1,21 +1,55 @@
 <?php
-class Cconexion{
-    
-    function conexionphp(){
-        $server = 'localhost';
-        $user = 'root';
-        $pass = '';
-        $based = 'hackatonweb';
+$server = 'localhost';
+$user = 'root';
+$pass = '';
+$based = 'hackatonweb';
 
-        try{
-            $conectar = new PDO($server, $user, $pass, $based);
-            echo"Conexion exitosa con la bd"           
-        } catch(PDOException $exp) {
-            echo("No se logro la conexion con: $based, error: $exp")
-        }
-        return $conexionphp;
+try {
+    $pdo = new PDO("mysql:host=$server;dbname=$based", $user, $pass);
+
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    $nombre_a_buscar = 'Bisuteria';
+
+    $sql = "SELECT * FROM especialidad WHERE nombre = :nombre";
+
+    $stmt = $pdo->prepare($sql);
+
+    $stmt->bindParam(':nombre', $nombre_a_buscar, PDO::PARAM_STR);
+
+    $stmt->execute();
+
+    $num_filas = $stmt->rowCount();
+
+    if ($num_filas > 0) {
+        echo "El registro con nombre '$nombre_a_buscar' existe en la tabla.";
+    } else {
+        echo "El registro con nombre '$nombre_a_buscar' no existe en la tabla.";
     }
-        
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
 }
-        
+
+try{
+    // Valor a insertar en la tabla
+    $nombre_especialidad = 'zapatero';
+
+    // Consulta SQL para insertar el nuevo registro
+    $sql = "INSERT INTO especialidad (nombre) VALUES (:nombre)";
+
+    // Preparar la consulta
+    $stmt = $pdo->prepare($sql);
+
+    // Asignar el valor a los parámetros de la consulta
+    $stmt->bindParam(':nombre', $nombre_especialidad, PDO::PARAM_STR);
+
+    // Ejecutar la consulta
+    $stmt->execute();
+
+    echo "Registro insertado correctamente.";
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+
+$pdo = null;
 ?>
